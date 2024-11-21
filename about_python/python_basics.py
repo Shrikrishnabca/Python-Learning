2534a707965d6b59d33cb6ad7bd21ab9ffa1a3a9
shrik <shrikrishnabca@gmail.com>
2024-11-21 20:39:48 +0530

    First day commit

---
+++ about_python/python_basics.py
@@ -0,0 +1,47 @@
+# Definition
+# Python is a high level programing language, it used to give the instruction to the computer to perform specific task.
+
+# History
+# Python was developed by Guido van Rossum in 1989
+# Python version 1.0 released in 1991
+# Python version 2.0 released in 2000
+# Python version 3.0 released in 2008
+
+# Why pyhon was popular
+# Python was free and open source (we can optimize based on our requirements)
+# Python is powerful object-oriented programing language(if we create anything is an object in python)
+# Python easy to read, write and understand(less syntax so we can more concentrate on logic)
+# Python come with a rich built-in library(it reduce the programmer time)
+# Python is system independent language(we can execute same code in any operating system)
+# Python is dynamic programing language(No need to specify data type automatically binds to appropriate data type during
+# run time)
+# Python is an interpreted language(Line by line execution)
+
+# Compilation
+# source_code.py -----> Python compiler---->byte_code.pyc-----> Pyton virtual machine -------> out put
+
+# Identifier
+# Name given to the different programing competes like class name, function name, variable name, object name, module
+
+# Rules to create identifier
+# Identifiers name can start with alphabet or _
+# It can contain alphanumeric value but not start with number
+# Special character not allowed except _
+# Keywords cannot be used as identifier
+
+# Keywords
+# Special words in python they have specific meaning to it, they are reserved words.
+# They are case-sensitive
+
+# Keywords in python
+# and - logical operation
+# as - to create alias
+# assert - for debugging
+# break - to break out of the loop
+# class - to define a class
+# continue - continue to next iteration of a loop
+# def - define a function
+
+
+
+
