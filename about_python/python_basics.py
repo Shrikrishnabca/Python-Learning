67ccbccd91f5dd4fae0cdb9aef7e514f1b76dac0
shrik <shrikrishnabca@gmail.com>
2024-11-21 20:39:48 +0530

    Second day commit

---
+++ about_python/python_basics.py
@@ -0,0 +1,79 @@
+# Definition
+# Python is a high level programing language, it used to give the instruction to the computer to perform specific task.
+
+# History
+# Python was developed by Guido van Rossum in 1989
+# Python version 1.0 released in 1991
+# Python version 2.0 released in 2000
+# Python version 3.0 released in 2008
+
+# Why pyhon was popular
+# Python was free and open source (we can optimize based on our requirements)
+# Python is powerful object-oriented programing language(if we create anything is an object in python)
+# Python easy to read, write and understand(less syntax so we can more concentrate on logic)
+# Python come with a rich built-in library(it reduce the programmer time)
+# Python is system independent language(we can execute same code in any operating system)
+# Python is dynamic programing language(No need to specify data type automatically binds to appropriate data type during
+# run time)
+# Python is an interpreted language(Line by line execution)
+
+# Compilation
+# source_code.py -----> Python compiler---->byte_code.pyc-----> Pyton virtual machine -------> out put
+
+# Identifier
+# Name given to the different programing competes like class name, function name, variable name, object name, module
+
+# Rules to create identifier
+# Identifiers name can start with alphabet or _
+# It can contain alphanumeric value but not start with number
+# Special character not allowed except _
+# Keywords cannot be used as identifier
+
+# Keywords
+# Special words in python they have specific meaning to it, they are reserved words.
+# They are case-sensitive
+
+# Keywords in python
+# and - logical operation
+# as - to create alias
+# assert - for debugging
+# break - to break out of the loop
+# class - to define a class
+# continue - continue to next iteration of a loop
+# def - define a function
+# del - delete object
+# elif - to make a condition statement
+# else - to make a condition statement
+# except - used in exception handling
+# False - boolean value
+# finally - used in exception handling
+# for - create loop
+# from - import specific part of the module
+# global - to declare the global variable
+# if - to make a condition statement
+# import - import specific module
+# in - used to check value present in iterable
+# is - used to check varietals are equal
+# lambda - create an anonymous function
+# None - represent the null value
+# nonlocal - to declare a nonlocal variable
+# not - logical operator
+# or - logical operator
+# pass - function without implementation or null statements
+# raise - to raise an exception
+# return -to exit the function and return a value
+# True - boolean value
+# try - exception handling
+# while - to create a loop
+# with - exception handling
+# yield - return a list of value from the generator
+
+
+# Variable
+# name given to the memory location and which holds the actual value.
+# value can be changed at any time
+
+
+
+
+
