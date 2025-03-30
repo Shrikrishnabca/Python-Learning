dc36c52e980132257aa822f192b36cf2c3304ae5
shrik <shrikrishnabca@gmail.com>
2025-03-30 23:45:08 +0530

    day to day learning

---
+++ Data_type/data_type_dictionary.py
@@ -0,0 +1,67 @@
+# Collection of kye and value pair
+# Each pair has unique keys
+# Key must be immutable or hashable
+# Value can be duplicate
+# Dictionary is mutable
+
+# Dictionary creation
+a = dict()
+print(a)  # {}
+
+c = dict(apple= 1, mango=20)
+print(c)
+
+b = {"apple": 1, "mango": 2}
+print(b)
+
+# Dictionary can have tuple as key
+a = {(1,2): "number", (1.0, 2.0): "floating"}
+
+# Accessing the value of dictionaries
+a = {"apple": 1, "mango": 2}
+print(a.get("apple"))  #  Returns the value for specified key
+
+print(a["apple"])  # Kind of indexing using key
+
+# Adding/ updating the dictionary
+a["banana"] = 5
+print(a)  # {'apple': 1, 'mango': 2, 'banana': 5}
+
+# Updating the existing the dictionary with specified item
+a.update({"grapes": 3, "jackfruit": 1})
+print(a)  # {'apple': 1, 'mango': 2, 'banana': 5, 'grapes': 3, 'jackfruit': 1}
+a.update(moosambi=2)
+print(a)  # {'apple': 1, 'mango': 2, 'banana': 5, 'grapes': 3, 'jackfruit': 1, 'moosambi': 2}
+
+a.setdefault("orange")  # For the kye set default value None, if it is not given
+print(a)  # {'apple': 1, 'mango': 2, 'banana': 5, 'grapes': 3, 'jackfruit': 1, 'moosambi': 2, 'orange': None}
+
+# Create dictionary with specified itable and value
+print(a.fromkeys([1,2], [11]))
+
+# total item in a dictionary
+print(len(a))  # 7
+
+# Deleting the key and value
+# pop() remove the specified item from the dictionary, return the value of removed element
+# Raise key error if specified kye not present
+print(a.pop("orange"))
+
+# popitem() remove the last added item return in the tuple formate
+print(a)
+print(a.popitem())  # ('moosambi', 2)
+
+# Merging the dictionary
+a = {"apple": 1, "mango": 2}
+b = {"naga": 1, "madhu": 2}
+c = { **a, **b}
+print(c)
+
+
+
+
+
+
+
+
+
--- Data_type/data_type_string.py
+++ Data_type/data_type_string.py
@@ -1,6 +1,11 @@
 # String is a collection off characters
 # String is immutable - cannot change the original object
 # String is bond with '', "", """"""
+import re
+
+from Data_type.data_type_list import words
+from about_python.built_in_functions import value
+
 print('Helo world')
 print("Hello world")
 print("""Hello world""")
@@ -117,6 +122,152 @@ a = "[1,2,3]"
 print(list(a))  # it will treat each as a char ['[', '1', ',', '2', ',', '3', ']']
 a = "123"
 print(list(a))
+
+# Write a program to reverse a string
+a = "hello"
+print(a[::-1])
+
+def reverse_string(word: str):
+    return word[::-1]
+print(reverse_string("shrikrishna"))
+
+# Check if the string is palindrome
+def is_palindrome(word: str):
+    return word == word[::-1]
+print( "given word is palindrome" if is_palindrome("mom") else "given word is not palindrome")
+
+# Count the occurrence of charactor
+def count_char(s, char):
+    return s.count(char)
+
+print("shrikrishna".count("s"))  # 2
+
+def char_count(word, char):
+    count = 0
+    for ch in word:
+        if ch == char:
+            count += 1
+    return count
+print(char_count("shrikrishn", "s"))  # 2
+
+# Find the no repeated character
+word = "hello"
+for ch in word:
+    if word.count(ch) == 1:
+        print(ch)
+
+# remove the duplicate from a string
+word = "hello"
+removed_word = ""
+for ch in word:
+    if word.count(ch) == 1:
+        removed_word += ch
+print(removed_word)
+
+def remove_duplicate(word: str):
+    return "".join(set(word))
+print(remove_duplicate("shrikrishna"))  # snhirka
+
+# Count the number of words in a string
+def count_word(s: str):
+    return len(s.split())   # list of words
+print(count_word("Hello world"))  # 2
+
+# Check two strings are anagrams
+def is_anagram(s1, s2):
+    return sorted(s1) == sorted(s2)
+print(is_anagram("listen", "silent"))  # True
+
+# Capitalize the First Letter of Each Word (Title Case)
+def title_case(s):
+    return s.title()
+
+print(title_case("hello world"))  # Hello World
+
+# Replace a Word in a String
+def replace(s, old, new):
+    return s.replace(old, new)
+print(replace("I love you", "love", "like"))  # I like you
+
+# Remove Spaces from a String
+print("hello world".replace(" ", ""))  # helloworld
+
+# Check if a String Contains Only Digits
+def is_number(s):
+    return s.isdigit()
+print(is_number("123"))  # True
+print(is_number("hello23"))  # False
+
+# Convert a String to ASCII Values
+def string_to_ascii(s):
+    return [ord(ch) for ch in s]
+print(string_to_ascii("hello"))
+
+# Convert ASCII value to string
+def ascii_to_string(values_list):
+    return "".join(chr(value) for value in values_list)
+print(ascii_to_string([104, 101, 108, 108, 111]))
+
+# Count Vowels and Consonants in a String
+
+
+def count_vowel(s: str):
+    v_count = 0
+    c_count = 0
+    vowel = "aeiouAEIOU"
+    for ch in s:
+        if ch in vowel:
+            v_count += 1
+        elif ch not in vowel and ch.isalpha():
+            c_count += 1
+    return v_count, c_count
+print(count_vowel("hello good morning"))
+
+def count_vowel(s: str):
+    vowel = "aeiouAEIOU"
+    v_count = sum(1 for ch in s if ch in vowel)
+    c_count = sum(1 for ch in s if ch not in vowel and ch.isalpha())
+    return v_count, c_count
+
+print(count_vowel("hello good morning"))
+
+# Remove Special Characters from a String
+
+def remove_spcl_characters(s: str):
+    spcl_chr = "!@#$%^&*()_+{}|:,./?';"
+    result_str = ""
+    for ch in s:
+        if ch not in spcl_chr:
+            result_str += ch
+    return result_str
+print(remove_spcl_characters("hello @ good morning#"))
+
+def remove_spcl(s):
+    return re.sub(r"[^A-za-z0-9]", " ", s)
+
+print(remove_spcl("hello @ good morning"))
+
+#  Find the Longest Word in a Sentence
+def long_word(s: str):
+    split_list = s.split()
+    max_len_str = ""
+    for ch in split_list:
+        if len(ch)> len(max_len_str):
+            max_len_str = ch
+    return max_len_str
+
+print(long_word("hello shrikishna gowda heggar"))
+
+# Swap Case of Each Letter in a String
+print("hello world".swapcase())
+
+# Print Each Character of a String Separately
+def print_ch(s: str):
+    for ch in s:
+        print(ch)
+print_ch("hello good morning")
+
+
 
 
 
